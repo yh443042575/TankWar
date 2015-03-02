06cd961ccece4967cd43cb4393a60d506a7d8e85
yh443042575 <443042575@qq.com>
2015-03-02 15:16:13 +0800

    更新0.7版本文件
    坦克可以以8个方向移动

---
+++ src/Tank.java
@@ -0,0 +1,127 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+public class Tank {
+
+	private final static int XSPEED = 5;
+	private final static int YSPEED = 5;
+	private boolean bL = false, bU = false, bD = false, bR = false;
+
+	enum Direction {
+		L, R, U, D, LU, LD, RU, RD, STOP
+	};
+
+	private Direction dir = Direction.STOP;
+	int x, y;
+
+	public Tank(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void draw(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(x, y, 30, 30);
+		g.setColor(c);
+	}
+
+	private void move() {
+		switch (dir) {
+		case L:
+			x -= XSPEED;
+			break;
+		case R:
+			x += XSPEED;
+			break;
+		case U:
+			y -= YSPEED;
+			break;
+		case D:
+			y += YSPEED;
+			break;
+		case LU:
+			x -= XSPEED;
+			y -= YSPEED;
+			break;
+		case LD:
+			x -= XSPEED;
+			y += YSPEED;
+			break;
+		case RU:
+			x += XSPEED;
+			y -= YSPEED;
+			break;
+		case RD:
+			x += XSPEED;
+			y += YSPEED;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_LEFT:
+			bL = true;
+			break;
+		case KeyEvent.VK_RIGHT:
+			bR = true;
+			break;
+		case KeyEvent.VK_UP:
+			bU = true;
+			break;
+		case KeyEvent.VK_DOWN:
+			bD = true;
+			break;
+		}
+		locateDirection();
+		move();
+		
+	}
+
+	private void locateDirection() {
+		if (!bL & !bR & !bU & !bD)
+			dir = Direction.STOP;
+		else if (bL & !bR & !bU & !bD)
+			dir = Direction.L;
+		else if (!bL & bR & !bU & !bD)
+			dir = Direction.R;
+		else if (!bL & !bR & bU & !bD)
+			dir = Direction.U;
+		else if (!bL & !bR & !bU & bD)
+			dir = Direction.D;
+		else if (bL & !bR & bU & !bD)
+			dir = Direction.LU;
+		else if (bL & !bR & !bU & bD)
+			dir = Direction.LD;
+		else if (!bL & bR & bU & !bD)
+			dir = Direction.RU;
+		else if (!bL & bR & !bU & bD)
+			dir = Direction.RD;
+
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_LEFT:
+			bL = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			bR = false;
+			break;
+		case KeyEvent.VK_UP:
+			bU = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			bD = false;
+			break;
+		}
+		
+	}
+}
---
+++ src/TankClient.java
@@ -0,0 +1,97 @@
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankClient extends Frame {
+
+	public static final int GAME_WIDTH=800, GAME_HEIGHT=600;
+	Tank myTank=new Tank(50, 50);
+	Image offScreenImage = null;
+
+	@Override
+	public void paint(Graphics g) {
+		
+		myTank.draw(g);
+		
+
+	}
+
+	public void update(Graphics g) {
+		if (offScreenImage == null) {
+			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+		}
+		
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c =gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+
+	}
+
+	public void lauchFrame() {
+		this.setLocation(400, 300);
+		this.setSize(GAME_WIDTH, GAME_HEIGHT);
+		this.setBackground(Color.GREEN);
+		setTitle("TankVar");
+		this.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+
+		});
+		this.addKeyListener(new KeyMonitor());
+		setVisible(true);
+		setResizable(false);
+
+		new Thread(new PaintThread()).start();
+	}
+
+	public static void main(String[] args) {
+		TankClient tankClient = new TankClient();
+		tankClient.lauchFrame();
+
+	}
+
+	private class PaintThread implements Runnable {
+
+		@Override
+		public void run() {
+			while (true) {
+				repaint();
+				try {
+					Thread.sleep(20);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+	
+	private class KeyMonitor extends KeyAdapter{
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			myTank.keyReleased(e);
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			myTank.keyPressed(e);
+			}
+		}
+		
+	}
+
+
